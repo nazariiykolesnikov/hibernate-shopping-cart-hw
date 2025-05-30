adea3148f50962d0171f7cfbed39ea6056c572ec
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-05-31 02:21:45 +0300

    first commit

--- src/main/java/mate/academy/security/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/security/AuthenticationServiceImpl.java
@@ -5,7 +5,9 @@ import mate.academy.exception.AuthenticationException;
 import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
+import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
 import mate.academy.util.HashUtil;
 
@@ -13,6 +15,8 @@ import mate.academy.util.HashUtil;
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
     private UserService userService;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
